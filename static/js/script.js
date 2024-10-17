aa14ec26d1b7b9e3de87d3b6bf3598a4cc093d0e
Luis Becerra <luis.becerra.15.12@gmail.com>
2024-10-17 11:36:10 -0300

    aumento de multiplicador

--- static/js/script.js
+++ static/js/script.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const timePassed = now - startDate;
         const percentagePassed = (timePassed / totalDuration) * 100;
 
-        const scaleFactor = 1 + (percentagePassed / 100 * 6);
+        const scaleFactor = 1 + (percentagePassed / 100 * 10);
         document.getElementById('background-image').style.transform = `scale(${scaleFactor})`;
 
         // If the countdown is over, display a message
